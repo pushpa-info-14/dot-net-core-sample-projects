dc6210dbb980863d578f2cbac21a89d111535766
pushpa-info-14 <pushpa.info.14@calcey.com>
2024-04-03 18:31:38 +0530

    dot net 8 authentication example

---
+++ DotNet8Authentication/Data/DataDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8Authentication.Data
+{
+    public class DataDbContext : IdentityDbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
