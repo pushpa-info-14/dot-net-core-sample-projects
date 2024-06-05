35f6210dd998652d68cc47d7adb11dd20aabcc57
pushpa-info-14 <pushpa.info.14@gmail.com>
2024-06-05 10:40:00 +0530

    clean architecture skeleton

---
+++ CleanArchitecture/Skeleton/Persistence.SqlServer/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence.SqlServer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistenceSqlServer(this IServiceCollection services)
+    {
+        return services;
+    }
+}
